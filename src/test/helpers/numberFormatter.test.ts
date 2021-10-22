4976e09869e20c193bde35129ab11e18cf954c9c
Krunal <kunalmasiyava@gmail.com>
2021-10-21 20:30:37 -0400

    fix: test cases

--- src/test/helpers/numberFormatter.test.ts
+++ src/test/helpers/numberFormatter.test.ts
@@ -4,21 +4,42 @@ const numberValue = 12;
 const stringValue = "25";
 const floatNumber = 50.548549468964;
 
+const formattedCADNumberValue = "CA$12.00";
+const formattedCADStringValue = "CA$25.00";
+const formattedCADFloatNumberValue = "CA$50.55";
+
 const formattedNumberValue = "$12.00";
 const formattedStringValue = "$25.00";
 const formattedFloatNumberValue = "$50.55";
 
-const _NaN = '$NaN';
+const _NaN = "$NaN";
+const CAD_NaN = "CA$NaN";
+
+const { locale } = Intl.DateTimeFormat().resolvedOptions();
 
 describe("Currency Formatter Func", () => {
-  it("should return formatted cad value", () => {
-    expect(currencyFormatter(numberValue)).toBe(formattedNumberValue);
-    expect(currencyFormatter(stringValue)).toBe(formattedStringValue);
-    expect(currencyFormatter(floatNumber)).toBe(formattedFloatNumberValue);
-});
+  if (locale === "en-CA") {
+    it("should return formatted cad value", () => {
+      expect(currencyFormatter(numberValue)).toBe(formattedNumberValue);
+      expect(currencyFormatter(stringValue)).toBe(formattedStringValue);
+      expect(currencyFormatter(floatNumber)).toBe(formattedFloatNumberValue);
+    });
 
-  it('should fail', () => {
-    expect(currencyFormatter(undefined)).toBe(_NaN);
-    expect(currencyFormatter(null)).toBe(_NaN);
-  })
+    it("should fail with NAN string", () => {
+      expect(currencyFormatter(undefined)).toBe(_NaN);
+      expect(currencyFormatter(null)).toBe(_NaN);
+    });
+  } else {
+    it("should return formatted cad value for according to US", () => {
+        expect(currencyFormatter(numberValue)).toBe(formattedCADNumberValue);
+        expect(currencyFormatter(stringValue)).toBe(formattedCADStringValue);
+        expect(currencyFormatter(floatNumber)).toBe(formattedCADFloatNumberValue);
+      });
+    
+      it("should fail", () => {
+        expect(currencyFormatter(undefined)).toBe(CAD_NaN);
+        expect(currencyFormatter(null)).toBe(CAD_NaN);
+      });
+  }
+  
 });
