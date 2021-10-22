208c72235729731b371f92d570187110b4c11c8b
Krunal <kunalmasiyava@gmail.com>
2021-10-22 18:13:44 -0400

    fix: css styling and accessibility

--- src/components /form/paymentCalculator.tsx
+++ src/components /form/paymentCalculator.tsx
@@ -1,4 +1,11 @@
-import { Grid, InputAdornment, MenuItem, TextField } from "@mui/material";
+import {
+  Grid,
+  InputAdornment,
+  MenuItem,
+  TextField,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import React from "react";
 import { IPaymentPlan } from "../../@types/types";
 import {
@@ -23,6 +30,8 @@ export const PaymentCalculator = (props: {
   handleChange: Function;
   handleSelect: Function;
 }) => {
+  const theme = useTheme();
+  const isTablet: boolean = useMediaQuery(theme.breakpoints.up("sm"));
   const { paymentPlan, dispatch, handleChange, handleSelect } = props;
   const paymentFrequency = [
     { label: "Weekly", value: 52 },
@@ -37,11 +46,12 @@ export const PaymentCalculator = (props: {
           toolTipTitle={PAYMENT_AMOUNT_TOOLTIP}
           title="Mortgage Amount:"
         />
+
         <Grid item md={6}>
           <TextField
             type="number"
             id="mortgage-amount"
-            aria-labelledby="mortgage amount"
+            aria-label="mortgage amount"
             value={paymentPlan.mortgageAmount}
             onChange={handleChange(MORTGAGE_AMOUNT, dispatch)}
             InputProps={{
@@ -60,7 +70,7 @@ export const PaymentCalculator = (props: {
           <TextField
             type="number"
             id="interest-rate"
-            aria-labelledby="annual mortgage interest rate"
+            aria-label="annual mortgage interest rate"
             value={paymentPlan.interestRate}
             onChange={handleChange(INTEREST_RATE, dispatch)}
             InputProps={{
@@ -73,50 +83,61 @@ export const PaymentCalculator = (props: {
           toolTipTitle={PAYMENT_PLAN_AMORTIZATION_TOOLTIP}
           title="Amortization Period:"
         />
-        <Grid item md={3}>
-          <TextField
-            select
-            name="years"
-            id="amortization-years"
-            aria-labelledby="mortgage amortization years"
-            size="small"
-            value={paymentPlan.amortizationPeriod.years}
-            onChange={handleSelect(AMORTIZATION_PERIOD, dispatch)}
-          >
-            {[...Array(31).keys()].map((i) => (
-              <MenuItem key={i} value={i}>
-                {i} Years
-              </MenuItem>
-            ))}
-          </TextField>
+        <Grid
+          item
+          container
+          direction="row"
+          spacing={2}
+          md={6}
+          xs={isTablet ? 6 : 12}
+          wrap="nowrap"
+        >
+          <Grid item>
+            <TextField
+              select
+              name="years"
+              id="amortization-years"
+              aria-label="mortgage amortization years"
+              size="small"
+              value={paymentPlan.amortizationPeriod.years}
+              onChange={handleSelect(AMORTIZATION_PERIOD, dispatch)}
+            >
+              {[...Array(31).keys()].map((i) => (
+                <MenuItem key={i} value={i}>
+                  {i} Years
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item>
+            <TextField
+              select
+              name="months"
+              id="mortgage-amount"
+              aria-label="mortgage amount"
+              size="small"
+              defaultValue={0}
+              value={paymentPlan.amortizationPeriod.months}
+              onChange={handleSelect(AMORTIZATION_PERIOD, dispatch)}
+            >
+              {[...Array(12).keys()].map((i) => (
+                <MenuItem key={i} value={i}>
+                  {i} months
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
         </Grid>
-        <Grid item md={3}>
-          <TextField
-            select
-            name="months"
-            id="mortgage-amount"
-            aria-labelledby="mortgage amount"
-            size="small"
-            defaultValue={0}
-            value={paymentPlan.amortizationPeriod.months}
-            onChange={handleSelect(AMORTIZATION_PERIOD, dispatch)}
-          >
-            {[...Array(12).keys()].map((i) => (
-              <MenuItem key={i} value={i}>
-                {i} months
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
+
         <ToolTipLabel
           toolTipTitle={PAYMENT_PLAN_FREQUENCY}
           title="Payment Frequency:"
         />
-        <Grid item md={6} xs={6}>
+        <Grid item md={6} xs={isTablet ? 6 : 12}>
           <TextField
             select
             id="payment-frequency"
-            aria-labelledby="payment frequency"
+            aria-label="payment frequency"
             value={paymentPlan.paymentFrequency.value}
             onChange={handleSelect(PAYMENT_FREQUENCY, dispatch)}
             variant="outlined"
@@ -134,7 +155,7 @@ export const PaymentCalculator = (props: {
           <TextField
             select
             id="mortgage-term"
-            aria-labelledby="mortgage term"
+            aria-label="mortgage term"
             size="small"
             value={paymentPlan.term}
             onChange={handleChange(TERM, dispatch)}
